403c4f37e14510ae96203e820ba61c93cc1db5e3
Giovanni Leo <2232254@slu.edu.ph>
2024-05-08 20:46:40 +0800

    latest commit

--- admin/src/vanniinfoinfo/view/panel/AdminHomePanel.java
+++ admin/src/vanniinfoinfo/view/panel/AdminHomePanel.java
@@ -12,17 +12,22 @@ import java.awt.*;
 public class AdminHomePanel extends JPanel {
 
     public AdminHomePanel(AdminControllerObserver adminControllerObserver) {
+        setBackground(Color.white);
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBackground(Color.white);
         JLabel title = new JLabel("WELCOME TO ADMIN DASHBOARD");
         title.setFont(FontFactory.newPoppinsBold(30));
         title.setAlignmentX(CENTER_ALIGNMENT);
         Picture picture = new Picture("resources/images/login-header.png", 450, 200);
+        picture.setBackground(Color.white);
         JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonsPanel.setBackground(Color.white);
         Button performer = new Button("Performers", new Dimension(115, 50), FontFactory.newPoppinsBold(15));
         Button liveSet = new Button("Livesets", new Dimension(115, 50), FontFactory.newPoppinsBold(15));
 
 
+        panel.add(Box.createVerticalStrut(150));
         panel.add(title);
         panel.add(picture);
         panel.add(buttonsPanel);
@@ -32,7 +37,6 @@ public class AdminHomePanel extends JPanel {
 
         performer.addActionListener(e -> adminControllerObserver.changeFrame(AdminPanel.PERFORMER));
         liveSet.addActionListener(e -> adminControllerObserver.changeFrame(AdminPanel.LIVE_SET));
-
     }
 
 }
